1c393f2224d949da9a32fcd60f7963c06d1b8120
nogumbi <nogumbi@student.wethinkcode.co.za>
2022-06-01 10:54:34 +0200

    Greeting controller added

--- rest-service/src/main/java/com/example/restservice/Greeting.java
+++ rest-service/src/main/java/com/example/restservice/Greeting.java
@@ -1,25 +1,20 @@
 package com.example.restservice;
 
 public class Greeting {
+
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content){
-        /**
-         * id field is for the unique identifier for the greeting
-         * content is the textual representation of the greeting
-         **/
+    public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
     }
 
-    public long getId(){
-
+    public long getId() {
         return id;
     }
 
-    public String getContent(){
+    public String getContent() {
         return content;
     }
-    
 }
